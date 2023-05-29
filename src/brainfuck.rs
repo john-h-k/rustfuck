d2318718db384e73ec896f87ede0830c485606b1
John Kelly <johnharrykelly@gmail.com>
2023-05-30 00:55:12 +0100

    Update brainfuck.rs

--- src/brainfuck.rs
+++ src/brainfuck.rs
@@ -94,6 +94,44 @@ impl IrGen {
     }
 }
 
+
+#[derive(Debug)]
+pub struct BrainfuckState {
+    pub cells: Vec<u8>,
+    pub pos: usize,
+}
+
+impl BrainfuckState {
+    pub fn new() -> Self {
+        Self { cells: Vec::new(), pos: 0 }
+    }
+    
+    pub fn read_cell(&self, i: usize) -> u8 {
+        // If the cell is OOB, it cannot have been written to, so must be zero
+        *self.cells.get(i).unwrap_or(&0u8)
+    }
+
+    pub fn read_cur_cell(&self) -> u8 {
+        self.read_cell(self.pos)
+    }
+
+    fn set_cur_cell(&mut self, val: u8) {
+        if self.pos >= self.cells.len() {
+            self.cells.resize(self.pos + 1, 0);
+        }
+
+        self.cells[self.pos] = val;
+    }
+
+    fn modify_cur_cell_with(&mut self, f: impl Fn(&mut u8)) {
+        if self.pos >= self.cells.len() {
+            self.cells.resize(self.pos + 1, 0);
+        }
+
+        f(&mut self.cells[self.pos]);        
+    }
+}
+
 pub struct HirInterpreter;
 
 impl HirInterpreter {
@@ -101,6 +139,7 @@ impl HirInterpreter {
         let table = Self::gen_branch_table(program);
 
         let mut instr_pointer = 0;
+        let mut state = BrainfuckState::new();
         
         Ok(())
     }
