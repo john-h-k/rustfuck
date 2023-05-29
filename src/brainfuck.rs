8b4d52bc392a6097468fe439bd24eb798452d0a3
John Kelly <johnharrykelly@gmail.com>
2023-05-30 00:49:27 +0100

    wip

--- src/brainfuck.rs
+++ src/brainfuck.rs
@@ -1,28 +1,37 @@
+use std::str::CharIndices;
+
 use bumpalo::Bump;
 
 pub struct IrGen {
     program: Vec<u8>,
 }
 
-pub enum IrOp {
+/// Represents a "real" brainfuck operation before optimisation
+#[derive(Debug, PartialEq, Eq)]
+pub enum BfOp {
     Inc,
     Dec,
     MvRight,
     MvLeft,
     In,
     Out,
+
+    BrFor,
+    BrBack,
 }
 
-pub enum IrLink<'ir> {
-    Forward(&'ir IrBlock<'ir>),
-    Back(&'ir IrBlock<'ir>),
-    End
-}
+/// Represents operations after the first opt pass
+/// * +- have been collapsed
+/// * >< have been collapsed
+#[derive(Debug)]
+pub enum HirOp {
+    Modify(isize), // add or subtract
+    Move(isize), // left or right movement
+    In,
+    Out,
 
-pub struct IrBlock<'ir> {
-    ir: Vec<IrOp>,
-
-    next: IrLink<'ir>,
+    BrFor,
+    BrBack,
 }
 
 impl IrGen {
@@ -31,32 +40,70 @@ impl IrGen {
     }
 
     pub fn gen(&mut self) -> () {
-        let mut bump = Bump::new();
-
-        let mut last_block = None;
-
         let mut ir = Vec::new();
         
         for command in self.program {
             match command {
-                b'+' => ir.push(IrOp::Inc),
-                b'-' => ir.push(IrOp::Dec),
-                b'>' => ir.push(IrOp::MvRight),
-                b'<' => ir.push(IrOp::MvLeft),
-                b'.' => ir.push(IrOp::Out),
-                b',' => ir.push(IrOp::In),
-
-                br @ b'[' | br @ b']' => {
-                    let next = match br => {
-                        b'[' => IrLink::Forward()
-                    }
-                    let block = bump.alloc(IrBlock { ir })
-                }
+                b'+' => ir.push(BfOp::Inc),
+                b'-' => ir.push(BfOp::Dec),
+                b'>' => ir.push(BfOp::MvRight),
+                b'<' => ir.push(BfOp::MvLeft),
+                b'.' => ir.push(BfOp::Out),
+                b',' => ir.push(BfOp::In),
+                b'[' => ir.push(BfOp::BrFor),
+                b']' => ir.push(BfOp::BrBack)
 
                 _ => continue
             }
         }
 
         todo!()
+    }
+
+    fn lower(bf: &[BfOp]) -> Vec<HirOp> {
+        let mut result = Vec::new();
+
+        let iter = bf.iter().peekable();
+
+        while let Some(bf_op) = iter.peek() {
+            let hir_op = match bf_op {
+                BfOp::Inc | BfOp::Dec => {
+                    let mod_ops = iter.take_while(|op| matches!(op, BfOp::Inc | BfOp::Dec));
+
+                    let delta = mod_ops.map(|&op| if op == BfOp::Inc { 1 } else { -1 }).sum();
+
+                    HirOp::Modify(delta)
+                },
+                BfOp::MvRight | BfOp::MvLeft => {
+                    let mod_ops = iter.take_while(|op| matches!(op, BfOp::MvRight | BfOp::MvLeft));
+
+                    let delta = mod_ops.map(|&op| if op == BfOp::MvRight { 1 } else { -1 }).sum();
+
+                    HirOp::Move(delta)
+                },
+                BfOp::In => HirOp::In,
+                BfOp::Out => HirOp::Out,
+                BfOp::BrFor => HirOp::BrFor,
+                BfOp::BrBack => HirOp::BrBack,
+            };
+
+            result.push(hir_op);
+        }
+
+        result
+    }
+}
+
+pub struct HirInterpreter;
+
+impl HirInterpreter {
+    pub fn execute(program: &[HirOp]) -> Result<()> {
+        
+    }
+
+    fn gen_branch_table(program: &[HirOp]) -> Result<Vec<usize>> {
+        let mut table = vec![0; program.len()];
+
+        for command in 
     }
 }
